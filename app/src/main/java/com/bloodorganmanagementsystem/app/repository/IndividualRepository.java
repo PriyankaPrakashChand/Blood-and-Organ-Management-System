20433ef6df4d7df2da62d0f5df8a0ba4c3f14718
PriyankaPrakashChand <53374732+PriyankaPrakashChand@users.noreply.github.com>
2020-04-29 15:57:47 +0400

    repositories added

---
+++ app/src/main/java/com/bloodorganmanagementsystem/app/repository/IndividualRepository.java
@@ -0,0 +1,17 @@
+package com.bloodorganmanagementsystem.app.repository;
+
+import com.bloodorganmanagementsystem.app.entities.Individual;
+import com.microsoft.azure.spring.data.cosmosdb.repository.CosmosRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IndividualRepository extends CosmosRepository<Individual, String> {
+
+public Optional<Individual> findById(String id);
+public  List<Individual> findByEmail(String email); 
+List<Individual> findByAppliedLicenseKey(String appliedLicenseKey);
+}
